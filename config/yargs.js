c3d1ff3c27422bfc5f48585f102091082882ff6c
Ganimedes Developer <ganimedes.developer@gmail.com>
2021-01-18 23:57:06 -0500

    Primer commit

---
+++ config/yargs.js
@@ -0,0 +1,27 @@
+const descripcion = {
+    demand: true,
+    alias: 'd'
+}
+
+const completado = {
+    default: true,
+    alias: 'c'
+}
+
+const argv = require('yargs')
+    .command('crear', 'Crea un elemento por hacer', {
+        descripcion
+    })
+    .command('actualizar', 'Actualiza el estado completado de una tarea', {
+        descripcion,
+        completado
+    })
+    .command('borrar', 'Borra una tarea', {
+        descripcion
+    })
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
